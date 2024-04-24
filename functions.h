908a81f0ab9b5955443b535def4b27ee922e661c
Thxnderbolt <153844870+Thxnderbolt@users.noreply.github.com>
2024-04-24 19:20:50 +0530

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,4 +5,11 @@
 #define MAX_STUDENTS 100
 #define FILENAME "marks.txt" // File name to store marks
 
+void addMarks(Student students[], int *count);
+void updateMarks(Student students[], int count);
+void deleteMarks(Student students[], int *count);
+void displayMarks(Student students[], int count);
+char calculateGrade(float percentage);
+void saveMarksToFile(Student students[], int count);
+
 #endif
